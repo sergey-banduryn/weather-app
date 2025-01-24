881b8fb3f1b8f37c80ee7af7e4282ba906903568
Sergey Banduryn <sergey_b89@i.ua>
2025-01-24 19:21:48 +0200

    feat: add prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * @type {import('prettier').Config}
+ */
+
+const config = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 80,
+  quoteProps: 'preserve',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  endOfLine: 'lf',
+};
+
+export default config;
